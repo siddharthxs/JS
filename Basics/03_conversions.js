3c31ca8d825b5d06b7ef8a3ec6dbd36585a1839c
Siddharth parmar <siddharthp.codes@gmail.com>
2025-07-27 15:14:50 +0000

    added conversion

---
+++ Basics/03_conversions.js
@@ -0,0 +1,52 @@
+let score = "33abc" 
+
+let valueInNumber = Number(score)
+
+//console.log(typeof score);
+
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+
+//"33" => 33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+
+
+//let isLoggedIn = "siddharth"   true
+//let isLoggedIn = ""
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+//let value = 0
+
+
+//let booleanValue = Boolean(value)
+
+//console.log(booleanValue);
+
+//let anynumber = 55
+
+//let  stringNumber = String(anynumber)
+
+//console.log(typeof stringNumber);
+
+//let anyvalue = "1235"
+
+//let converted = BigInt(anyvalue)
+
+//console.log(converted);
+
+
+
+// ************************************** Operations ***********************************************
+
+
+let value = 5
+
+let negValue = -value
+
+console.log(negValue);
