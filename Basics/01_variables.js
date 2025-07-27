7079b8ea23d5d57dd7e889a65740dbadc412a99f
siddharthxs <siddharthp.codes@gmail.com>
2025-07-27 09:28:35 +0000

    added variables and datatypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountID = 155268
+let accountEmail="siddhathhp.codes@gmail.com"
+var accountPassword="294916166"
+accountCity="delhi"
+
+// accountID="555644"
+
+console.log(accountID);
+
+accountEmail="example@gmail.com"
+accountPassword="helloworld"
+accountCity="jaipur"
+let accountstate;
+
+console.table([accountEmail, accountID, accountPassword, accountCity,accountstate])
+
+
+/*
+   prefer not to use var 
+   because of issue in block scope and functional scope 
+
+*/
+
+/* we have to remember one more thing that in javascript we can declare a variable by not putting var before it.
+although we should not do that beccuse it may create problem  further.*/
+
+/* in js using semicolon is not necessary its totally your choice wether you want to use it or not*/
---
+++ Basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; //to treat all the js code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+//code readibility should be high 
+
+console.log( 3 + 3)
+
+console.log("Siddharth")
+
+
+let name="siddharth"
+let age=18
+let city="delhi"
+let state;
+
+console.log(typeof 18)
+
+
+/*   
+   Data types in java script 
+   there are two types of Data types in javascript : Primitive Data types and Non primitive Datatypes
+
+
+   #primitive Data types 
+   1.Number      eg: 43,3.14
+   2.bigint      eg: 464464644555164946464
+   3.String      eg: "hello","abc"
+   4.boolean     eg: true, false 
+   5.null        eg: let x= null;
+   6.undefined   eg: let AccountId;
+   7.symbol      eg: symbol('id')
+
+
+   #Non-primitive Data types
+   1.object
+*/
